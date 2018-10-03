7547f75c43cc0ef82faec0cf44e0b3d47d1629ff
github username <github email address>
2018-10-03 09:37:23 -0400

    testing new stuff

--- src/index.js
+++ src/index.js
@@ -10,18 +10,12 @@ document.addEventListener('DOMContentLoaded', () => {
   let buyForm = document.querySelector("#buy-stock-form")
   let sellForm = document.querySelector("#sell-stock-form")
 
-
   //SWAP THIS OUT LATER WITH SIGNED IN USERS
-  let user;
-
   User.findByUsername("Dummy")
     .then((foundUser) => {
       let user = new User(foundUser)
       user.renderUserProfile();
-      let balancediv = document.querySelector('#balance-info')
-      balancediv.innerHTML = `Balance: $${user.money}`
       app = new App(user)
-
     })
     .then(() => {
 
@@ -29,6 +23,72 @@ document.addEventListener('DOMContentLoaded', () => {
 
         app.adapter.fetchCompanies().then(app.createCompanies)
 
+        document.querySelector("#title").addEventListener("click", ()=>{
+          $('.ui.labeled.icon.sidebar').sidebar('toggle');
+        })
+        document.querySelector("#login_button").addEventListener("click", ()=>{
+          $('#login_modal').modal('show');
+        })
+        document.querySelector("#signup_button").addEventListener("click", ()=>{
+          $('#signup_modal').modal('show');
+        })
+        document.querySelector("#signup_link").addEventListener("click", ()=>{
+          $('#signup_modal').modal('show');
+        })
+
+
+
+      //change this later to be the user's stocks
+      /*
+
+          COMMENTED THIS OUT BECAUSE CAN'T OVERWRITE EXISTING COMPANIES
+
+      */
+      // let companies_symbol = ["GE","BB","AMD","NFLX","GOOGL"]
+      // companies_symbol.forEach(function(symbol){
+      //     app.adapter.getStock(symbol).then(company => {
+      //
+      //       let data = {description: company.quote.sector,
+      //                   price: company.quote.latestPrice,
+      //                   news: company["news"][0]["headline"],
+      //                   name: company.quote.companyName,
+      //                   symbol: company.quote.symbol,
+      //                   open_price: company.quote.open,
+      //                   close_price: company.quote.close,
+      //                   high: company.quote.high,
+      //                   low: company.quote.low
+      //                   }
+      //       app.adapter.postCompany(data)
+      //         .then((companyObj) => {
+      //           let company = new Company(companyObj)
+      //           profile.append(company.renderCompany());
+      //         })
+      //         .catch(error => {
+      //           debugger
+      //         })
+      //
+      //
+      //
+      //
+      //       // this.id = data.id
+      //       // this.description = data.description
+      //       // this.price = data.price
+      //       // this.name = data.name
+      //       // this.symbol = data.symbol
+      //       // this.adapter = new Adapter()
+      //
+      //       // Company.all.push(this)
+      //       //I want to make the below function a class function, must organize BETTER
+      //       //stocks should be made into Company forms first then rendered
+      //       // companyList.append(renderCompany())
+      //       // profile.append(renderCard(company))
+      //   })
+      //
+      // })
+
+
     })
+
+
 
 }) //end of DOM content loaded
