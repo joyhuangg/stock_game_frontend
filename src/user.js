3e05e4399f6e41ffca292cca83b496a21b4a6981
joyhuangg <joyhuangg@gmail.com>
2018-10-03 09:36:39 -0400

    added scrollbar

--- src/user.js
+++ src/user.js
@@ -28,18 +28,26 @@ class User{
 
   renderUserProfile(){
     let profileDiv = document.querySelector("#profile")
+          // <div id=​"scrolling-wrapper-flexbox">​
+          //
+          // ​</div>​
     let toAppend = `
       <h1>Welcome ${this.name}</h1>
-      <h2>${this.username}</h2>
+      <h2>Username: ${this.username}</h2>
       <h3>Balance: $${this.money}</h3>
+      <div id="scrolling-wrapper-flexbox">
+
+      </div>
     `
 
-    profileDiv.innerHTML = toAppend
+
+    profileDiv.innerHTML += toAppend
+    let scrollmenu = document.querySelector('#scrolling-wrapper-flexbox')
     this.adapter.getUser(this.id)
       .then((user) => {
         user.cards.forEach(function(stock){
           let stockObj = new StockCard(stock).renderCard()
-          profileDiv.append(stockObj)
+          scrollmenu.append(stockObj)
         })
       })
     // this.companies.forEach(function(company){
