7c2b4e453849558207d65d0a941ff8fdaf690d6c
github username <github email address>
2018-10-03 12:11:11 -0400

    fixed card

--- src/stock_card.js
+++ src/stock_card.js
@@ -1,24 +1,36 @@
+// t.integer "user_id"
+// t.integer "company_id"
+// t.float "quantity"
+// t.float "buy_price"
+// t.float "sell_price"
+
+
 class StockCard{
   constructor(data){
       this.id = data.id
       this.adapter = new Adapter()
       this.user = data.user
       this.company = data.company
+      this.quantity = data.quantity
       this.buy_price = data.buy_price
       this.sell_price = data.sell_price
       StockCard.all.push(this)
   }
 
   renderCard(){
+    // debugger
     let new_card = document.createElement("div")
     new_card.className += "card"
-    new_card.dataset.id = this.id
-    new_card.innerHTML =`<h1>${this.company.name} (${this.company.symbol})</h1>
-    <button data-id=${this.id}>Sell</button>
-    <h3>Buy Price: $${this.buy_price}</h3>
-    <h3>${this.description}</h3>
+    new_card.innerHTML =`<div class="ui one column centered grid" style="height:200px;">
+      <div class="top aligned row" style="height:50%"><h3>${this.company.name} (${this.company.symbol})</h3></div>
+      <div class="middle aligned row">Buy Price: $${this.buy_price}</div>
 
-    <p>News :${this.company.news}</p>
+        <div class="bottom aligned row" style="height:25%">
+          <button data-id=${this.id}>Buy</button>
+          <button data-id=${this.id}>Sell</button>
+        </div>
+      </div>
+    </div>
     `
 
     return new_card
