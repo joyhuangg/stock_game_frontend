2c903fdae8b7864a701ec41b85ef306623788b5c
joyhuangg <joyhuangg@gmail.com>
2018-11-14 21:07:29 -0500

    make it better

--- src/adapter.js
+++ src/adapter.js
@@ -1,6 +1,6 @@
 class Adapter{
   constructor(){
-    this.baseURL = 'http://localhost:3000/api/v1';
+    this.baseURL = 'https://git.heroku.com/stock-game-backend/api/v1';
     this.headers = {
       'Content-Type': 'application/json',
       Accept: 'application/json',
